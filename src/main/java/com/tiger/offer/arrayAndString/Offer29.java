feb9b16344a105af16f85d409d19d8034621f195
xushuaitiger <1084204930@qq.com>
2020-10-07 17:34:20 +0800

    #added junit test of offer 29

--- src/main/java/com/tiger/offer/arrayAndString/Offer29.java
+++ src/main/java/com/tiger/offer/arrayAndString/Offer29.java
@@ -3,7 +3,7 @@ package com.tiger.offer.arrayAndString;
 /**
  * 剑指 Offer 29. 顺时针打印矩阵
  * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
- *
+ * <p>
  * 思路
  * 标签：二维数组
  * 整体思路：循环遍历整个数组，循环中再嵌套四个循环，分别是从左至右，从上至下，从右至左，从下至上这几个方向，按照题意将整个数组遍历完成，控制好边界
@@ -16,7 +16,7 @@ package com.tiger.offer.arrayAndString;
  * 从上至下：遍历完成后 --right，如果 left > right​，到达边界循环结束
  * 从右至左：遍历完成后 --bottom，如果 top > bottom​，到达边界循环结束
  * 从下至上：遍历完成后 ++left，如果 left > right​，到达边界循环结束
- *
+ * <p>
  * 作者：画手大鹏
  * 链接：https://leetcode-cn.com/leetbook/read/illustrate-lcof/55v2m2/
  * 来源：力扣（LeetCode）
@@ -26,38 +26,42 @@ public class Offer29 {
 
     /**
      * 左右->上下->右左->下上，好个白鹤亮翅！
+     *
      * @param matrix
      * @return
      */
     public int[] spiralOrder(int[][] matrix) {
 
-        if(matrix.length == 0) return new int[0];
+        if (matrix.length == 0) return new int[0];
         int left = 0, right = matrix[0].length - 1, top = 0,
                 bottom = matrix.length - 1, x = 0;
 
         int[] res = new int[(right + 1) * (bottom + 1)];
-        while(true) {
+        while (true) {
             //左右
-            for(int i = left; i <= right; i++) res[x++] = matrix[top][i];
+            for (int i = left; i <= right; i++) res[x++] = matrix[top][i];
 
-            if(++top > bottom) break;
+            if (++top > bottom) break;
 
             //上下
-            for(int i = top; i <= bottom; i++) res[x++] = matrix[i][right];
+            for (int i = top; i <= bottom; i++) res[x++] = matrix[i][right];
 
-            if(left > --right) break;
+            if (left > --right) break;
 
             //右左
-            for(int i = right; i >= left; i--) res[x++] = matrix[bottom][i];
+            for (int i = right; i >= left; i--) res[x++] = matrix[bottom][i];
 
-            if(top > --bottom) break;
+            if (top > --bottom) break;
 
             //下上
-            for(int i = bottom; i >= top; i--) res[x++] = matrix[i][left];
+            for (int i = bottom; i >= top; i--) res[x++] = matrix[i][left];
 
-            if(++left > right) break;
+            if (++left > right) break;
         }
         return res;
     }
+
+
+
 
 }
---
+++ src/test/java/com/tiger/util/Offer29Test.java
@@ -0,0 +1,28 @@
+package com.tiger.util;
+
+import com.tiger.offer.arrayAndString.Offer29;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Locale;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class Offer29Test {
+    private static Offer29 offer29;
+
+    @BeforeClass
+    public  static void starting() {
+        offer29 = new Offer29();
+    }
+
+    @Test
+    public void testspiralOrder() {
+        int[][] test = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        System.out.println("start offer 29 testing");
+        assertArrayEquals(offer29.spiralOrder(test),
+                new int[]{1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7});
+        System.out.println("test of offer 29 successfully passed!");
+    }
+}
