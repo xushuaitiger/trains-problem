ca5ee07315fdc91209023ca986ebb14c0b87f872
xushuaitiger <1084204930@qq.com>
2020-10-16 22:39:26 +0800

    #added offer09

---
+++ src/main/java/com/tiger/offer/stackAndQueue/Offer09.java
@@ -0,0 +1,40 @@
+package com.tiger.offer.stackAndQueue;
+
+import java.util.Stack;
+
+/**
+ * 剑指 Offer 09. 用两个栈实现队列
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ * 作者：画手大鹏
+ * 链接：https://leetcode-cn.com/leetbook/read/illustrate-lcof/xz8cid/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class Offer09 {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public Offer09() {
+        stack1 = new Stack<Integer>();
+        stack2 = new Stack<Integer>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.isEmpty()) {
+            return -1;
+        } else {
+            return stack2.pop();
+        }
+    }
+
+
+}
