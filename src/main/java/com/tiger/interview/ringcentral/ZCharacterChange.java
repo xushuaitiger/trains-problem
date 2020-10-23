c114bb3e22aa2b09575f45276ef6a1fac7d18300
xushuaitiger <1084204930@qq.com>
2020-10-23 22:43:02 +0800

    #字符串z型变换

---
+++ src/main/java/com/tiger/interview/ringcentral/ZCharacterChange.java
@@ -0,0 +1,54 @@
+package com.tiger.interview.ringcentral;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ * <p>
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ * <p>
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ZCharacterChange {
+
+    public String convert(String s, int numRows) {
+        if(numRows < 2) {
+            return s;
+        }
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+        /**
+         * l    c
+         * e  t o  e
+         * e    d
+         */
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if(i == 0 || i == numRows -1){
+                flag = - flag;
+            }
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) {
+            res.append(row);
+        }
+        return res.toString();
+    }
+
+
+    public static void main(String[] args){
+        String test = "leetcode";
+        System.out.println(new ZCharacterChange().convert(test,3));
+    }
+}
